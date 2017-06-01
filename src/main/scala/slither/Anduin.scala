dbfea85cdbd28c84464533e9890c949da07fde01
Francesco Perera <fp244@cornell.edu>
2017-06-01 00:04:01 -0400

    added Main interface

--- src/main/scala/slither/Anduin.scala
+++ src/main/scala/slither/Anduin.scala
@@ -1,7 +1,10 @@
 package slither
 
+import io.circe.Json
 import sources.Source
 import sources.Format
+
+import scalaj.http.{Http, HttpRequest, HttpResponse}
 
 
 class Anduin( source: Source, transformation: Any) {
@@ -9,6 +12,31 @@ class Anduin( source: Source, transformation: Any) {
 
   private val URL: String = "http://localhost:8081/stream"
 
-  def transform( format: Format) = ???
+  // for now let it return the body as string, but it will be a a list of NDJSON objects
+  def transform( format: Format): String = {
+    val response: HttpResponse[String] = Http(URL).postData(buildPostData(source.toMap)).method("POST").header("Content-Type","application/json;charset=utf-8").asString
+    response.body
+  }
+
+  // return the JSON body in String format
+  def buildPostData(sourceMap: Map[String,String]): String = {
+    val rawData: String = s"""
+                            {
+                                  "source":${sourceMap.toString} ,
+                                  "pipelines": [
+                                  {
+                                    "target": {
+                                      "_type": "DirectResponse",
+                                      "format": "Csv"
+                                    },
+                                    "transformations":[],
+                                    "pipelines": []
+                                  }
+                                  ]
+                                }
+                          """
+    rawData
+
+  }
 
 }
