9e879c0115c140557b9159befcd44c1d3d9479a7
Francesco Perera <fp244@cornell.edu>
2017-06-01 19:45:24 -0400

    LiterateTomato getting data from APIs. Need to add response parsing to make the output more friendly

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
-import slither.Anduin
-import slither.sources.{CSV, S3Source}
+import slither.literatetomatoAPI.{AddressSegmentation, ColumnOntology, DateTranslation}
+import slither.{Anduin, LiterateTomato}
+import slither.sources.{Csv, S3Source}
 
 object Main extends App {
   val dummyS3: S3Source = S3Source("AKIAIG66OKZETK3MWTEA",
@@ -9,7 +10,14 @@ object Main extends App {
     "PII/test/test.json"
   )
 
-  val anduin: Anduin = new Anduin(dummyS3,"xyz")
-  val data = anduin.transform(CSV)
-  println(data)
+//  val anduin: Anduin = new Anduin(dummyS3,"xyz")
+//  val data = anduin.transform(Csv)
+//  println(data)
+  val name = Vector("Francesco Perera")
+  val output = LiterateTomato.get(ColumnOntology,name)
+  val output2 = LiterateTomato.get(AddressSegmentation, Vector("5 West 119th Apartment 2, New York, New York , 10026"))
+  val output3 = LiterateTomato.get(DateTranslation,Vector("29th Jan 1993"))
+  println(output)
+  println(output2)
+  println(output3)
 }
--- src/main/scala/slither/LiterateTomato.scala
+++ src/main/scala/slither/LiterateTomato.scala
@@ -1,16 +1,37 @@
 package slither
 
+import com.typesafe.scalalogging.LazyLogging
+import io.circe.{Json, JsonObject}
 import slither.literatetomatoAPI.LiterateTomatoAPI
+import io.circe.syntax._
 
 import scalaj.http.{Http, HttpResponse}
 
 
-object LiterateTomato {
+object LiterateTomato extends LazyLogging {
 
-  def get(callType: LiterateTomatoAPI, dataVector: String) = {
-    val response: HttpResponse[String] = Http(callType.url).postData(dataVector).method("POST").headers(Seq(( "Cache-Control", "no-cache"),( "Content-Type","application/json"))).asString
-    response.body
+  def get(callType: LiterateTomatoAPI, dataVector: Vector[String]) = {
+
+    val postData: Option[JsonObject] = jsonifyPostData(dataVector)
+    postData match {
+      case Some(obj) =>
+        val stringObj: String = obj.asJson.toString()
+        val response: HttpResponse[String] = Http(callType.url).postData(stringObj).method("POST").headers(Seq(( "Cache-Control", "no-cache"),( "Content-Type","application/json"))).asString
+        response.body
+      case None =>  None
+    }
   }
+
+  def jsonifyPostData(dataVector:Vector[String]): Option[JsonObject] = {
+    val optJsonData: Option[Vector[Json]] = dataVector.asJson.asArray
+    optJsonData match {
+      case Some(jsonVec) => Map("data" -> jsonVec).asJson.asObject
+      case None =>
+        logger.error(s"$dataVector cannot be encoded to Json Array.")
+        None
+    }
+  }
+
 
 
 }
