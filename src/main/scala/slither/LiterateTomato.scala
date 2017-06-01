de736c85d9e33251d1f80755a334fa5d9e31dada
Francesco Perera <fp244@cornell.edu>
2017-06-01 16:38:11 -0400

    defined get call for LiterateTomato. Call needs to be tested

--- src/main/scala/slither/LiterateTomato.scala
+++ src/main/scala/slither/LiterateTomato.scala
@@ -7,8 +7,8 @@ import scalaj.http.{Http, HttpResponse}
 
 object LiterateTomato {
 
-  def get(callType: LiterateTomatoAPI, dataVector: Vector[String]) = {
-    val response: HttpResponse[String] = Http(callType.url).postData("").method("POST").header("Content-Type","application/json").asString
+  def get(callType: LiterateTomatoAPI, dataVector: String) = {
+    val response: HttpResponse[String] = Http(callType.url).postData(dataVector).method("POST").headers(Seq(( "Cache-Control", "no-cache"),( "Content-Type","application/json"))).asString
     response.body
   }
 
